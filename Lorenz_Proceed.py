b5611ff4ae73170fcb76ef5a1cec6cff86cccaca
blh119 <holliday4434@gmail.com>
2023-11-12 15:25:46 -0500

    need to fix the list index error

--- Lorenz_Proceed.py
+++ Lorenz_Proceed.py
@@ -12,26 +12,8 @@ sigma = 10.
 beta = 8/3
 rho = 28.
 
-mu0 = 100.
-mu2 = 100. 
-
-
-def linear_space(start, end, steps):
-    
-    step_length = (end - start) / steps
-
-    even_spaced_nums = [start]
-    
-    # keeps track if we have reached 
-    tracker = start
-    
-    # add values to the even_spaced_nums list
-    while tracker < end:
-        tracker = tracker + step_length
-        even_spaced_nums.append(tracker)
-        
-    return np.array(even_spaced_nums) # return whole list as numpy array
-
+mu0 = 1000.
+mu2 = 1000. 
 
 def get_ic(x0, y0, z0): 
     
@@ -74,77 +56,101 @@ def get_ic_list(x0, y0, z0):
     return x_list, y_list, z_list
 
 
-def solve_lorenz_split_1(time_step, tf, init_x, init_y, init_z, last_sol_list, last_ndg_list, counter):
+def solve_lorenz_split_1_first(time_step, init_x, init_y, init_z):
     
     dt = 0.0001
     
     sol = np.empty((time_step, 3))   
     ndg = np.empty((time_step, 3))
-    g = np.empty(time_step)
-    
-    if counter == 0:
+    sol_g = np.empty(time_step)
+    ndg_g = np.empty(time_step)
         
-        init_x, init_y, init_z = get_ic(60., 59.9, 10.)
+    sol[0] = np.array([init_x,
+                       init_y, 
+                       init_z])
         
-        sol[0] = np.array([sigma*(init_y - init_x), 
-                           rho*init_x - init_y - init_x*init_z,
-                           init_x*init_y - beta*init_z])
+    ndg[0] = np.array([init_x, 
+                       0., # nudging equation for y starts at zero
+                       init_z])
         
+    sol_g[0] = -init_x*init_z
         
-        ndg[0] = np.array([sigma*(init_y - init_x), 
-                           rho*init_x - init_y - 0., # zero is just a placeholder
-                           init_x*init_y - beta*init_z])
-        g[0] = 0.
-        
-    else:
-        
-        sol[0] = np.array([last_sol_list[0,0], last_sol_list[0,1], last_sol_list[0,2]])
-        ndg[0] = np.array([last_ndg_list[0,0], last_ndg_list[0,1], last_ndg_list[0,2]])
-        g[0] = -last_ndg_list[0,0]*last_ndg_list[0,2]
-        
-    
-    print("Time Final: ", tf, "Time Step: ", time_step)
+    ndg_g[0] = 0.
+
     
     for i in range(1, time_step):
         
         u = sol[i-1]
         v = ndg[i-1]
         
-        # set g value
-        if counter == 0:
-            g_ = 0.
-            
-        else:
-    
-            last_ndg = last_ndg_list[i-1]
-            g_ = -last_ndg[0]*last_ndg[2]
+        ndg_g_ = 0.
         
         u_ = np.array([sigma*(u[1]-u[0]), rho*u[0]-u[1]-u[0]*u[2], u[0]*u[1]-beta*u[2]])
-    
         # unknown solution
-        v_ = np.array([sigma*(v[1]-v[0])-mu0*(v[0]-u[0]), rho*v[0]-v[1]+g_, v[0]*v[1]-beta*v[2]-mu2*(v[2]-u[2])])
-        #v_ = f_ + np.array([, , 0])
+        v_ = np.array([sigma*(v[1]-v[0])-mu0*(v[0]-u[0]), rho*v[0]-v[1]+ndg_g_, v[0]*v[1]-beta*v[2]-mu2*(v[2]-u[2])])
         
         sol[i] = u + (dt*u_)
         ndg[i] = v + (dt*v_)
-        g[i] = g_
+        sol_g[i] = -u[0]*u[2]
+        ndg_g[i] = ndg_g_
+        
+        print(sol[i], ndg[i], sol_g[i], ndg_g[i])
+        
+    return [np.array(sol), np.array(ndg), np.array(sol_g), np.array(ndg_g)]
+
+
+def solve_lorenz_split_1(time_step, init_x, init_y, init_z, init_ndg_y, ndg_list):
     
-    return [sol, ndg, g]
+    dt = 0.0001
+    
+    sol = np.empty((time_step, 3))   
+    ndg = np.empty((time_step, 3))
+    sol_g = np.empty(time_step)
+    ndg_g = np.empty(time_step)
+    
+    sol[0] = np.array([init_x, 
+                       init_y, 
+                       init_z])
+    ndg[0] = np.array([init_x, 
+                       init_ndg_y, 
+                       init_z])
+    
+    sol_g[0] = -init_x*init_z
+    ndg_g[0] = -ndg_list[0][0]*ndg_list[0][2]
+    
+    for i in range(1, time_step):
+        
+        u = sol[i-1]
+        v = ndg[i-1]
+        
+        u_ = np.array([sigma*(u[1]-u[0]), rho*u[0]-u[1]-u[0]*u[2], u[0]*u[1]-beta*u[2]])
+        # unknown solution
+        v_ = np.array([sigma*(v[1]-v[0])-mu0*(v[0]-u[0]), rho*v[0]-v[1]+ndg_g[i-1], v[0]*v[1]-beta*v[2]-mu2*(v[2]-u[2])])
+        
+        sol[i] = u + (dt*u_)
+        ndg[i] = v + (dt*v_)
+        sol_g[i] = -u[0]*u[2]
+        ndg_g[i] = -ndg_list[i][0]*ndg_list[i][2]
+        
+        print(sol[i], ndg[i], sol_g[i], ndg_g[i])
+        
+    return [np.array(sol), np.array(ndg), np.array(sol_g), np.array(ndg_g)]
         
 def nudged_system(tf, step):
     
     time_steps = np.arange(0, tf + 1, step)[::-1]
     time_steps = time_steps[time_steps != 0]
     
+    init_values_index = np.arange(0, tf, step)
+    init_values_index = init_values_index[init_values_index != 0]-1
+    
     sol_full_set = []
     ndg_full_set = []
-    g_full_set = []
+    sol_g_full_set = []
+    ndg_g_full_set = []
     
     # initial run
-    
     counter = 0 # this helps keep track of our correct initial position
-    
-    list_indexer = int(time_steps[0]-time_steps[1])
     
     for time_step in time_steps:
         
@@ -152,39 +158,42 @@ def nudged_system(tf, step):
             
             init_x, init_y, init_z = get_ic(60., 59.9, 10.)
             
-            current_sol, current_ndg, current_g = solve_lorenz_split_1(time_step, tf, init_x, init_y, init_z, np.nan, np.nan, counter)
+            current_sol, current_ndg, current_sol_g, current_ndg_g = solve_lorenz_split_1_first(time_step, init_x, init_y, init_z)
             
             sol_full_set.append(current_sol)
             ndg_full_set.append(current_ndg)
-            g_full_set.append(current_g)
+            sol_g_full_set.append(current_sol_g)
+            ndg_g_full_set.append(current_ndg_g)
+            
+            sol_init_points = current_sol[init_values_index]           
             
             counter = counter + 1
             
         else:
             
-            last_sol = sol_full_set[counter-1][list_indexer:]
-            last_ndg = ndg_full_set[counter-1][list_indexer:]
+            init_x = sol_init_points[counter-1, 0]
+            init_y = sol_init_points[counter-1, 1]
+            init_z = sol_init_points[counter-1, 2]
+            init_ndg_y = ndg_full_set[counter-1][step-1][1]
+            ndg_list = ndg_full_set[counter-1][step-1:]
             
-            print("Time Step: ", time_step, "last ndg size: ", len(last_ndg))
-            
-            current_sol, current_ndg, current_g = solve_lorenz_split_1(time_step, tf, init_x, init_y, init_z, last_sol, last_ndg, counter)
+            current_sol, current_ndg, current_sol_g, current_ndg_g = solve_lorenz_split_1(time_step, init_x, init_y, init_z, init_ndg_y, ndg_list)
             
             sol_full_set.append(current_sol)
             ndg_full_set.append(current_ndg)
-            g_full_set.append(current_g)
+            sol_g_full_set.append(current_sol_g)
+            ndg_g_full_set.append(current_ndg_g)
             
             counter = counter + 1
             
-        if counter == 1:
-            return sol_full_set, ndg_full_set, g_full_set
-        
-    return sol_full_set, ndg_full_set, g_full_set
+    return sol_full_set, ndg_full_set, sol_g_full_set, ndg_g_full_set
 
-
-def get_endpoints(sol, ndg):
+def get_endpoints(sol, ndg, sol_g, ndg_g):
     
     sol_endpoints = []
     ndg_endpoints = []
+    sol_g_endpoints = []
+    ndg_g_endpoints = []
     
     for i in range(len(sol)):
         sol_endpoints.append(sol[i][len(sol[i])-1])
@@ -192,13 +201,20 @@ def get_endpoints(sol, ndg):
     for i in range(len(ndg)):
         ndg_endpoints.append(ndg[i][len(sol[i])-1])
         
-    return np.array(sol_endpoints), np.array(ndg_endpoints)
+    for i in range(len(sol_g)):
+        sol_g_endpoints.append(sol_g[i][len(sol_g[i])-1])
+        
+    for i in range(len(ndg_g)):
+        ndg_g_endpoints.append(ndg_g[i][len(ndg_g[i])-1])
+        
+    return np.array(sol_endpoints), np.array(ndg_endpoints), np.array(sol_g_endpoints), np.array(ndg_g_endpoints)
 
-def plot_absolute_errors(sol_endpoints, ndg_endpoints, g_full_set):
+def plot_absolute_errors(sol_endpoints, ndg_endpoints, sol_g_endpoints, ndg_g_endpoints):
     
     abs_u = abs(sol_endpoints[:,0]-ndg_endpoints[:,0])
     abs_v = abs(sol_endpoints[:,1]-ndg_endpoints[:,1])
     abs_w = abs(sol_endpoints[:,2]-ndg_endpoints[:,2])
+    abs_g = abs(sol_g_endpoints-ndg_g_endpoints)
     
     t = np.arange(0, len(abs_u))
     
@@ -206,56 +222,21 @@ def plot_absolute_errors(sol_endpoints, ndg_endpoints, g_full_set):
     plt.plot(t, abs_u, lw=1, label=r'$|u|$') 
     plt.plot(t, abs_v, lw=1, label=r'$|v|$')
     plt.plot(t, abs_w, lw=1, label=r'$|w|$')
+    plt.plot(t, abs_g, lw=1, label=r'$|g|$')
     #plt.plot(self.t, self.delta_S, 'k', lw=1, label=r'$|\tilde{\sigma}-\sigma|$')
-    plt.title('Evolution of absolute errors'); plt.xlabel('t'); plt.yscale('log')
+    plt.title('Evolution of absolute errors'); plt.xlabel('t = 1000 td = 1 mu = 1000'); plt.yscale('log')
     plt.legend(); plt.grid(); plt.tight_layout()
     plt.show()
 
-sol, ndg, g = nudged_system(100000, 1000)
-sol_endpoints, ndg_endpoints = get_endpoints(sol, ndg)
-plot_absolute_errors(sol_endpoints, ndg_endpoints, g)
+sol, ndg, sol_g, ndg_g = nudged_system(1000, 1)
+sol_endpoints, ndg_endpoints, sol_g_endpoints, ndg_g_endpoints = get_endpoints(sol, ndg, sol_g, ndg_g)
+plot_absolute_errors(sol_endpoints, ndg_endpoints, sol_g_endpoints, ndg_g_endpoints)
 
-
-plt.plot(np.arange(100000), sol[0][:, 0], label = "x")
-plt.plot(np.arange(100000), sol[0][:, 1], label = "y")
-plt.plot(np.arange(100000), sol[0][:, 2], label = "z")
-plt.legend()
-plt.show()
-            
-            
-plt.plot(np.arange(91000), sol[9][:, 0], label = "x")
-plt.plot(np.arange(91000), sol[9][:, 1], label = "y")
-plt.plot(np.arange(91000), sol[9][:, 2], label = "z")
-plt.legend()
-plt.show()           
-            
-x_list, y_list, z_list = get_ic_list(60., 59.9, 10.)
-
-
-plt.plot(np.arange(40000, 50002), x_list[40000:], label = "x")
-plt.plot(np.arange(40000, 50002), y_list[40000:], label = "y")
-plt.plot(np.arange(40000, 50002), z_list[40000:], label = "z")
-plt.legend()
-plt.show()   
         
+t = np.arange(0, len(ndg_g_endpoints))
 
-plt.plot(np.arange(80000, 100002), x_list[80000:], label = "x")
-plt.plot(np.arange(80000, 100002), y_list[80000:], label = "y")
-plt.plot(np.arange(80000, 100002), z_list[80000:], label = "z")
-plt.legend()
-plt.show()  
-
-plt.plot(np.arange(100002), x_list, label = "x")
-plt.plot(np.arange(100002), y_list, label = "y")
-plt.plot(np.arange(100002), z_list, label = "z")
-plt.legend()
+plt.plot(t, sol_g_endpoints, lw=1, label=r'$|s_g|$')
+plt.plot(t, ndg_g_endpoints, lw=1, label=r'$|n_g|$')
+plt.title('Evolution of absolute errors'); plt.xlabel('t = 1000 td = 1 mu = 1000')
 plt.show()
 
-
-plt.plot(np.arange(1,101), sol_endpoints[:, 0], label = "x")
-plt.plot(np.arange(1,101), sol_endpoints[:, 1], label = "y")
-plt.plot(np.arange(1,101), sol_endpoints[:, 2], label = "z")
-plt.legend()
-plt.show()  
-        
-        
